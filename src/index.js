e209fa4d9ea35710c4695b36ce175d98b9b8f2f8
Matias Gabriel Martini <matias.martini.gabriel@gmail.com>
2020-06-09 20:41:05 -0300

    iniciando proyecto

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+
+import Home from './pages/Home'
+
+
+
+const container = document.getElementById('app');
+
+ReactDOM.render(<Home />, container)
