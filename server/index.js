86ae743ebe708128692712d709dced130b388a06
Matias Gabriel Martini <matias.martini.gabriel@gmail.com>
2020-06-16 21:31:47 -0300

    arreglos

--- server/index.js
+++ server/index.js
@@ -23,17 +23,17 @@ if (!isDev && cluster.isMaster) {
   const app = express();
 
   // Priority serve any static files.
-  app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
+  app.use(express.static(path.resolve(__dirname, '../app-react/build')));
 
   // Answer API requests.
-  app.get('/api', function (req, res) {
+  app.get('/', function (req, res) {
     res.set('Content-Type', 'application/json');
     res.send('{"message":"Hello from the custom server!"}');
   });
 
   // All remaining requests return the React app, so it can handle routing.
   app.get('*', function(request, response) {
-    response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
+    response.sendFile(path.resolve(__dirname, '../app-react/build', 'index.html'));
   });
 
   app.listen(PORT, function () {
